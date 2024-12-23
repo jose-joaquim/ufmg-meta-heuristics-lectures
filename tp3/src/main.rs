f0fdb38c2d3945ed943c4aed19120ced195b8e72
José Joaquim de Andrade Neto <jjandradent@gmail.com>
2024-12-22 22:31:53 -0300

    tp3

--- tp3/src/main.rs
+++ tp3/src/main.rs
@@ -3,7 +3,6 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-use rand::seq::IteratorRandom;
 use rand::seq::SliceRandom;
 use rand::Rng;
 use std::collections::HashSet;
@@ -15,7 +14,6 @@ type Individual = (f64, Vec<usize>);
 type Population = Vec<Individual>;
 
 fn read_and_build_adjacency_matrix(file_path: &str) -> Option<Vec<Vec<f64>>> {
-    // println!("Reading Instance");
     let input: Vec<String> = fs::read_to_string(file_path)
         .unwrap()
         .lines()
@@ -26,7 +24,6 @@ fn read_and_build_adjacency_matrix(file_path: &str) -> Option<Vec<Vec<f64>>> {
     for i in 6..input.len() - 1 {
         let mut parts = input[i].trim().split_whitespace();
         let words: Vec<&str> = input[i].trim().split_whitespace().collect();
-        println!("{:?}", words);
 
         let x: f64 = words[1].parse().unwrap();
         let y: f64 = words[2].parse().unwrap();
@@ -49,17 +46,14 @@ fn read_and_build_adjacency_matrix(file_path: &str) -> Option<Vec<Vec<f64>>> {
         }
     }
 
-    // println!("Read instance");
-
     return Some(adj);
 }
 
-fn stop_criteria_reached(foo: i32) -> bool {
-    return true;
+fn stop_criteria_reached(start: Instant) -> bool {
+    return start.elapsed().as_secs_f64() > 15.0;
 }
 
 fn selection(pop: &Population) -> Population {
-    // Binary Tournament Selection
     let mut random_select: Population = Vec::new();
     let mut rng = rand::thread_rng();
 
@@ -71,8 +65,76 @@ fn selection(pop: &Population) -> Population {
     random_select
 }
 
-fn mutation(individual: &Individual) -> Individual {
-    (0.0, Vec::new())
+fn mutation(individual: &mut Individual) {
+    let mut rng = rand::thread_rng();
+    let mut lb: usize = rng.gen_range(0..individual.1.len());
+    let mut ub: usize = rng.gen_range(0..individual.1.len());
+
+    (individual.1[lb], individual.1[ub]) = (individual.1[ub], individual.1[lb]);
+}
+
+fn all_unique(vec: Vec<i32>) -> bool {
+    let mut seen = HashSet::new();
+    vec.into_iter().all(|x| seen.insert(x))
+}
+
+fn crossover(parent1: &Individual, parent2: &Individual) -> Vec<Individual> {
+    let n: usize = parent1.1.len();
+    let (mut off1, mut off2): (Vec<usize>, Vec<usize>) = (vec![n + 1; n], vec![n + 1; n]);
+
+    let (lb, ub): (usize, usize) = {
+        let mut rng = rand::thread_rng();
+        let mut lb_: usize = rng.gen_range(0..n - 1);
+        let mut ub_: usize = rng.gen_range(0..n - 1);
+
+        while lb_ == ub_ {
+            ub_ = rng.gen_range(0..n - 1);
+        }
+
+        if lb_ > ub_ {
+            std::mem::swap(&mut lb_, &mut ub_);
+        }
+
+        (lb_, ub_)
+    };
+
+    let mut replacement1: Vec<i32> = vec![-1; n];
+    let mut replacement2: Vec<i32> = vec![-1; n];
+
+    for idx in lb..=ub {
+        off1[idx] = parent2.1[idx];
+        off2[idx] = parent1.1[idx];
+
+        replacement1[parent2.1[idx]] = parent1.1[idx] as i32;
+        replacement2[parent1.1[idx]] = parent2.1[idx] as i32;
+    }
+
+    for i in 0..n {
+        if i >= lb && i <= ub {
+            continue;
+        }
+
+        let mut n1: usize = parent1.1[i];
+        let mut m1: i32 = replacement1[n1];
+
+        while m1 != -1 {
+            n1 = m1 as usize;
+            m1 = replacement1[m1 as usize];
+        }
+
+        let mut n2 = parent2.1[i];
+        let mut m2 = replacement2[n2];
+
+        while m2 != -1 {
+            n2 = m2 as usize;
+            m2 = replacement2[m2 as usize];
+        }
+
+        off1[i] = n1;
+        off2[i] = n2;
+    }
+
+    vec![(0.0, off1), (0.0, off2)]
 }
 
 fn reproduction(pop: &Population) -> Population {
@@ -85,9 +147,9 @@ fn reproduction(pop: &Population) -> Population {
             (i1, i2)
         };
 
-        let children: Vec<Individual> = Vec::new();
+        let mut children: Vec<Individual> = crossover(&p1, &p2);
 
-        for child in children.iter() {
+        for child in &mut children {
             mutation(child);
             offspring.push((child.0, child.1.clone()));
         }
@@ -96,9 +158,14 @@ fn reproduction(pop: &Population) -> Population {
     offspring
 }
 
-fn evaluate_population(pop: &Population) {}
+fn evaluate_population(pop: &mut Population, adj: &Vec<Vec<f64>>) {
+    pop.iter_mut().for_each(|(val, route)| {
+        *val = route.windows(2).map(|l| adj[l[0]][l[1]]).sum();
+        *val += adj[route[route.len() - 1]][route[0]];
+    });
+}
 
-fn create_population(size: &i32, adj: &Vec<Vec<f64>>, start: usize) -> Population {
+fn create_population(size: &usize, adj: &Vec<Vec<f64>>, start: &usize) -> Population {
     (0..*size)
         .map(|_| {
             let n = adj.len();
@@ -116,20 +183,36 @@ fn create_population(size: &i32, adj: &Vec<Vec<f64>>, start: usize) -> Populatio
         .collect()
 }
 
-fn genetic_algorithm() -> Population {
-    let mut population: Population = Vec::new();
-    evaluate_population(&population);
+fn replacement(mut population: Population, mut offspring: Population) -> Population {
+    population.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+    offspring.extend(population[0..=1].into_iter().cloned());
+    offspring.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+    offspring[0..(offspring.len() - 2)].to_vec()
+}
+
+fn genetic_algorithm(
+    adj: &Vec<Vec<f64>>,
+    start: usize,
+    pop_size: usize,
+) -> (Population, Population) {
+    let mut population: Population = create_population(&pop_size, &adj, &start);
+
+    let init_pop: Population = population.clone();
+    let start = Instant::now();
     loop {
-        if stop_criteria_reached(10) {
+        if stop_criteria_reached(start) {
             break;
         }
 
-        selection(&population);
-        reproduction(&population);
-        evaluate_population(&population);
-    }
+        let mating = selection(&population);
+        let mut offspring = reproduction(&mating);
 
-    population
+        evaluate_population(&mut offspring, &adj);
+
+        population = replacement(population, offspring);
+    }
+    (init_pop, population)
 }
 
 fn main() {
@@ -137,14 +220,13 @@ fn main() {
 
     let adj = read_and_build_adjacency_matrix(&args[1]);
     let root: usize = 0;
-    let k_max: usize = 120;
+    let pop_size = 15;
 
-    // let ans: (Individual, f64, Individual, f64) = vnd(&adj.unwrap(), k_max, root);
+    let (init_pop, population) = genetic_algorithm(&adj.unwrap(), 0, pop_size);
 
-    // let (ch_sol, ch_time, vnd_sol, vnd_time): (Individual, f64, Individual, f64) =
-    //     vnd(&adj.unwrap(), k_max, root);
-    // println!(
-    //     "{} & {:.2} & {:.2} & {:.2} & {:.2} \\\\",
-    //     args[1], ch_sol.0, ch_time, vnd_sol.0, vnd_time
-    // );
+    println!("{}", args[1]);
+    println!("Initial_Population,Final_Population");
+    for i in 0..pop_size {
+        println!("{:.2},{:.2}", init_pop[i].0, population[i].0);
+    }
 }
